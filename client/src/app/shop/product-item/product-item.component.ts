2fa749afd6c7e12a68a4060f21a539c546598dbc
Billy Wang <wzz343@gmail.com>
2023-03-14 16:28:29 +1100

    Add pagination, sort, search filter

---
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/shared/models/product';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.scss']
+})
+export class ProductItemComponent implements OnInit {
+  // To receive an input property from its parent 
+  @Input() product: IProduct;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
