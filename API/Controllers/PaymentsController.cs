68c060af39822ce568306bd72cc066c5d7508de5
Billy <billy.wang.aus@gmail.com>
2023-10-14 12:00:41 +1100

    add Stripe webhook

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -3,16 +3,20 @@ using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 namespace API.Controllers
 {
     public class PaymentsController : BaseApiController
     {
         private readonly IPaymentService _paymentService;
+        private readonly ILogger<PaymentsController> _logger;
+        private const string WebhookSecret = "";
 
-        public PaymentsController(IPaymentService paymentService)
+        public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger)
         {
             _paymentService = paymentService;
+            _logger = logger;
         }
 
         [Authorize]
@@ -20,13 +24,39 @@ namespace API.Controllers
         public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
         {
             var basket = await _paymentService.CreateOrUpdatePaymentIntent(basketId);
-            
+
             if (basket == null)
             {
                 return BadRequest(new ApiResponse(400, "Problem with your basket"));
             }
-            
+
             return basket;
+        }
+
+        // Stripe webhook
+        // https://stripe.com/docs/webhooks#webhooks-def
+        [HttpPost("webhook")]
+        public async Task<ActionResult> StripeWebhook()
+        {
+            // read data from Stripe
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], WebhookSecret);
+
+            // Handle the event
+            if (stripeEvent.Type == Events.PaymentIntentSucceeded)
+            {
+                // Then define and call a method to handle the successful payment intent.
+                var paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+                _logger.LogInformation("Payment Succeeded: ", paymentIntent.Id);
+            }
+            else if (stripeEvent.Type == Events.PaymentIntentPaymentFailed)
+            {
+                var paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+                _logger.LogError("Payment Failed: ", paymentIntent.Id);
+            }
+
+            return new EmptyResult();
         }
     }
 }
